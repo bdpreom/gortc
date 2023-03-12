b7af7d9530f6bf3fb640e1caf42f5e25e5c52189
Shadman Sakib Khan <preom.ssk@gmail.com>
2023-03-12 18:10:59 +0600

    middleware push

--- internal/handlers/welcome.go
+++ internal/handlers/welcome.go
@@ -2,9 +2,6 @@ package handlers
 
 import "github.com/gofiber/fiber/v2"
 
-
-
 func Welcome(c *fiber.Ctx) error {
-	return c.Render("welcome",nil,"layouts/main")
-
+	return c.Render("welcome", nil, "layouts/main")
 }
